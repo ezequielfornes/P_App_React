b1232949acd1709e4fed38990a08bcf44809f43e
ezequiel <ezequielfornes@gmail.com>
2021-05-29 22:17:45 -0300

    t

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [maps, setMaps]= useState([]);
+  const [places, setPlaces]= useState([]);
+
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
